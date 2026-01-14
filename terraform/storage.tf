3cae7daef4e3bf68d920115fc07574f613fe0ad5
Monica Ghavalyan <monica.ghavalyan@arm-mac-lx9kfgkfxc.tail6b705.ts.net>
2026-01-14 14:00:04 +0400

    terraform backend

--- terraform/storage.tf
+++ terraform/storage.tf
@@ -2,6 +2,7 @@ resource "google_storage_bucket" "dataplex_sources" {
   name          = "${var.project_id}-dataplex-sources"
   location      = var.region
 
+  force_destroy = true
   uniform_bucket_level_access = true
 
   labels = {
